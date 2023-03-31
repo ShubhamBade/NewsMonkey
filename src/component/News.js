3cf726729caeeafe34e6cd94f05c58d6f66bfd61
Shubham <shubhambade3@gmail.com>
2023-03-31 22:02:50 +0530

    Next and Prevoius button functionality has been optimized

--- src/component/News.js
+++ src/component/News.js
@@ -8,18 +8,50 @@ constructor(){
     this.state={
         articles:[],
         loading: false
+
     }
 }
 async componentDidMount(){
-    let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=d4bee73e416e401bace2ab4f3e4f5cd5";
+    let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=d4bee73e416e401bace2ab4f3e4f5cd5&page=1&pageSize=20";
     let data=await fetch(url);
     let parseData=await data.json()
 
     this.setState(
         {
-            articles : parseData.articles
+            articles : parseData.articles,
+            page:1,
+            totalResults:parseData.totalResults
         }
     )
+}
+handlePrevoius=async()=>{
+  console.log("clicked on previous");
+  let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=d4bee73e416e401bace2ab4f3e4f5cd5&page=${this.state.page-1}&pageSize=20`;
+    let data=await fetch(url);
+    let parseData=await data.json()
+
+
+  this.setState({
+    page:this.state.page-1,
+    articles : parseData.articles
+  })
+}
+handleNext=async()=>{
+  console.log("clicked on next");
+  if(this.state.page> Math.ceil(this.state.totalResults/20)){
+    console.log("No content to show");
+  }
+  else{
+  let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=d4bee73e416e401bace2ab4f3e4f5cd5&page=${this.state.page+1}&pageSize=20`;
+    let data=await fetch(url);
+    let parseData=await data.json()
+
+
+  this.setState({
+    page:this.state.page+1,
+    articles : parseData.articles
+  })
+}
 }
 
   render() {
@@ -36,8 +68,8 @@ async componentDidMount(){
         })}
         </div>
         <div className="container d-flex justify-content-between">
-        <button type="button" class="btn btn-dark">&larr;Prevoius</button>
-        <button type="button" class="btn btn-dark">Next&rarr;</button>
+        <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevoius}>&larr;Prevoius</button>
+        <button disabled={this.state.page+1> Math.ceil(this.state.totalResults/20)} type="button" className="btn btn-dark" onClick={this.handleNext}>Next&rarr;</button>
         </div>
       </div>
     )
